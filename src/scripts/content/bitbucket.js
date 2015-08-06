5e991e7c09c0e2847fd2d8dfd37ddd9ba7cc1999
Mark Pavlitski <markpavlitski@gmail.com>
2015-08-06 13:51:48 +0100

    Extended BitBucket integration to include support for tracking pull requests.

--- src/scripts/content/bitbucket.js
+++ src/scripts/content/bitbucket.js
@@ -21,3 +21,23 @@ togglbutton.render('#issue-header', {}, function (elem) {
 
   $('#issue-header').appendChild(link);
 });
+
+togglbutton.render('#pullrequest', {}, function (elem) {
+  var link, description,
+    numElem = $('.pull-request-status > a', elem),
+    titleElem = $('.pull-request-title > h1', elem),
+    projectElem = $('.aui-page-header-main > h1 > a.entity-name');
+
+  description = titleElem.innerText;
+  if (numElem !== null) {
+    description = "PR-" + numElem.textContent.substr(1) + " " + description;
+  }
+
+  link = togglbutton.createTimerLink({
+    className: 'bitbucket-pr',
+    description: description,
+    projectName: projectElem && projectElem.textContent
+  });
+
+  $('.compare-widget-container', elem).appendChild(link);
+});
